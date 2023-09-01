67c6000b374907e5eab3b2d4b43101c697180a84
Pixelaks <86482414+anaghksatheesh@users.noreply.github.com>
2023-09-01 15:26:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import { HashRouter } from "react-router-dom";
+
+import React, { useEffect } from 'react';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <HashRouter>
@@ -10,4 +13,15 @@ root.render(
  </HashRouter>  
 
 );
+function App() {
+  useEffect(() => {
+    document.title = "Pixelaks Games";
+  }, []);
+
+  return (
+    <div>
+      <h1>Pixelaks Games!</h1>
+    </div>
+  );
+}
 
